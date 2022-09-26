caa2dc94001615f203b64d5210605b9669c1a878
Daniel Williams <williamsoligodaniel@gmail.com>
2022-09-26 05:25:52 +0000

    create a static folder with an js file and its components

---
+++ src/static/index.jsx
@@ -0,0 +1,24 @@
+const content = {
+  inputs: [
+    {
+      label:"Full Name",
+      name: "fullname",
+      type: "text",
+      placeholder: 'Enter your full name...'
+    },
+    {
+      label:"Email",
+      name: "email",
+      type: "email",
+      placeholder: 'Enter your email address...'
+    },
+    {
+      label:"password",
+      name: "password",
+      type: "password",
+      placeholder: 'Enter your password'
+    }
+  ]
+}
+
+export default content;
